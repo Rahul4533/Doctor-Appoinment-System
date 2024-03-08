4b820764b16cbf281e2be9d9d03750c206ec36f8
Rahul <Rahulsingh4533@gmail.com>
2024-03-08 11:17:16 +0530

    add start

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use('/doctor',require('./routes/doctorRoutes'));
 //listen server
  app.use(express.static(path.join(__dirname, '/client/build')));
 
- http.get('*',function(req,res){
+ app.get('*',function(req,res){
 const index=path.resolve(__dirname,'client','build','index.html');
    res.sendFile(index);
  })
