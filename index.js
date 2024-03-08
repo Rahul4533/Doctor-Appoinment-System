094e218869bab1a9596ec5d1c0717446ac263800
Rahul <Rahulsingh4533@gmail.com>
2024-03-08 10:18:11 +0530

    updated

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ app.use('/doctor',require('./routes/doctorRoutes'));
 //listen server
  app.use(express.static(path.join(__dirname, './client/build')));
 
- app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+ app.get('*',function(req,res){
+const index=path.join(__dirname,'./client/build/index.html');
+   res.sendFile(index);
  })
 
 
